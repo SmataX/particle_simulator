cf3fee4d4051af43c28410dd008835966b48a2e7
Mateusz Jaworski <jaworskimat.priv@gmail.com>
2025-01-02 22:34:51 +0100

    fixed favoriting one side when liquid is spreading

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 
 from grid import Grid
+from particle import PowderParticle, LiquidParticle
 
 class App:
     def __init__(self, WIDTH: int = 100, HEIGHT = 100, CELL_SIZE = 4):
@@ -9,11 +11,13 @@ class App:
         self.clock = pygame.time.Clock()
         self.running = True
         self.grid = Grid(WIDTH, HEIGHT)
+        self.cell_size = CELL_SIZE
 
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                    
 
             self.screen.fill("#000000")
 
@@ -23,8 +27,31 @@ class App:
 
             self.clock.tick(60)  # limits FPS to 60
 
+    def get_position_in_grid(self, position: tuple):
+        return [round(position[0] / self.cell_size), round(position[1] / self.cell_size)]
+
     def update(self):
-        pass
+        if pygame.mouse.get_pressed(3):
+            mouse_pos_in_grid = self.get_position_in_grid(pygame.mouse.get_pos())
+            if self.grid.is_within(mouse_pos_in_grid[0], mouse_pos_in_grid[1]):
+                self.grid.set_value(mouse_pos_in_grid[0], mouse_pos_in_grid[1], PowderParticle(colors=["#ffff00"]))
+
+        for y in range(self.grid.height - 1, 0, -1):
+            x_indices = list(range(self.grid.width))
+            random.shuffle(x_indices)  # Randomize the order of horizontal processing
+            for x in x_indices:
+                target_cell = self.grid.get_value(x, y)
+                if target_cell:
+                    target_cell.step(self.grid, x, y)
+
+        self.render_particles()
+
+    def render_particles(self):
+        for y in range(self.grid.height):
+            for x in range(self.grid.width):
+                target_cell = self.grid.get_value(x, y)
+                if target_cell:
+                    pygame.draw.rect(self.screen, target_cell.color, (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size))
 
 pygame.quit()
 
---
+++ src/particle.py
@@ -0,0 +1,65 @@
+import random
+
+from grid import Grid
+
+class PowderParticle:
+    def __init__(self, colors: list, density: int = 10):
+        self.color = random.choice(colors)
+        self.density = density
+
+    def step(self, grid: Grid, x: int, y: int):
+        if not grid.is_within(x, y + 1):
+            return
+        
+        target_cell = grid.get_value(x, y + 1)
+        if target_cell == None:
+            grid.swap_values(x, y, x, y + 1)
+            return
+        else:
+            if not grid.is_within(x - 1, y + 1):
+                return
+            target_cell = grid.get_value(x - 1, y + 1)
+            if target_cell == None or type(target_cell) == LiquidParticle:
+                grid.swap_values(x, y, x - 1, y + 1)
+                return
+            
+            if not grid.is_within(x + 1, y - 1):
+                return
+            target_cell = grid.get_value(x + 1, y + 1)
+            if target_cell == None:
+                grid.swap_values(x, y, x + 1, y + 1)
+                return
+
+class LiquidParticle:
+    def __init__(self, colors: list, density: int = 10):
+        self.color = random.choice(colors)
+        self.density = density
+    
+    def step(self, grid: Grid, x: int, y: int):
+        if grid.is_within(x, y + 1):
+            target_cell = grid.get_value(x, y + 1)
+            if target_cell == None:
+                grid.swap_values(x, y, x, y + 1)
+                return
+        
+        # Spread diagonally downwards if possible
+        downward_sides = []
+        if grid.is_within(x - 1, y + 1) and grid.get_value(x - 1, y + 1) is None:
+            downward_sides.append((x - 1, y + 1))
+        if grid.is_within(x + 1, y + 1) and grid.get_value(x + 1, y + 1) is None:
+            downward_sides.append((x + 1, y + 1))
+        if downward_sides:
+            target = random.choice(downward_sides)
+            grid.swap_values(x, y, target[0], target[1])
+            return
+
+        # Spread horizontally
+        sides = []
+        if grid.is_within(x - 1, y) and grid.get_value(x - 1, y) is None:
+            sides.append((x - 1, y))
+        if grid.is_within(x + 1, y) and grid.get_value(x + 1, y) is None:
+            sides.append((x + 1, y))
+        if sides:
+            # Simulate viscosity by randomly selecting a side
+            target = random.choice(sides)
+            grid.swap_values(x, y, target[0], target[1])
